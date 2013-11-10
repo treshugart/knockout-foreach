98f1110b5c57d352669f383c07332ded8ca656b8
Trey Shugart <treshugart@gmail.com>
2013-11-11 09:47:31 +1100

    Initial commit.

---
+++ src/foreach.js
@@ -0,0 +1,32 @@
+define(['knockout'], function(ko) {
+
+  function buildContext(obj, ctx) {
+    var props = [];
+
+    for (var a in obj) {
+      if (obj.hasOwnProperty(a)) {
+        props.push({
+          key: a,
+          val: obj[a]
+        });
+      }
+    }
+
+    return props;
+  };
+
+  ko.virtualElements.allowedBindings.each = true;
+
+  return {
+    init: function(el, value, bindings, model, context) {
+      ko.applyBindingsToNode(el, {
+        foreach: buildContext(ko.unwrap(value()), context)
+      });
+
+      return {
+        controlsDescendantBindings: true
+      };
+    }
+  };
+
+});
